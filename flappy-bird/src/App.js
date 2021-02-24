f62aa9106af7387f3d10e2c0e9006d7822b50562
Kontorta16 <75789956+Kontorta16@users.noreply.github.com>
2021-02-24 15:43:39 +0300

    Proje Bitirildi

--- flappy-bird/src/App.js
+++ flappy-bird/src/App.js
@@ -2,22 +2,30 @@ import React, { Component } from 'react';
 import { NeuralNetwork } from './neural/nn';
 import './App.css';
 
-const TOTAL_BIRDS = 100;
+const TOTAL_BIRDS = 1000;
 const HEIGHT = 500;
 const WIDTH = 800;
-const PIPE_WIDTH = 60;
+const PIPE_WIDTH = 80;
 const MIN_PIPE_HEIGHT = 40;
-const FPS = 120;
+const FPS = 960;
 
 class Bird {
-  constructor(ctx) {
+  constructor(ctx, brain) {
     this.ctx = ctx;
     this.x = 150;
     this.y = 150;
+    this.age = 0;
+    this.fitness = 0;
     this.gravity = 0;
     this.velocity = 0.1;
     this.isDead = false;
-    this.brain = new NeuralNetwork(2, 5, 1);
+    if (brain) {
+      this.brain = brain.copy();
+      this.mutate();
+    }
+    else {
+      this.brain = new NeuralNetwork(5, 10, 2);
+    }
   }
   draw() {
     this.ctx.fillStyle = 'red';
@@ -26,7 +34,8 @@ class Bird {
     this.ctx.fill();
   }
 
-  update = () => {
+  update = (pipeX, spaceStartY, spaceEndY) => {
+    this.age += 1;
     this.gravity += this.velocity;
     this.gravity = Math.min(4, this.gravity);
     this.y += this.gravity;
@@ -38,33 +47,47 @@ class Bird {
       this.y = HEIGHT;
     }
 
-    this.think();
+    this.think(pipeX, spaceStartY, spaceEndY);
   }
 
-  think = () => {
+  think = (pipeX, spaceStartY, spaceEndY) => {
     // Inputs
     // [bird.x , bird.y]
     // [closestPipe.x, pipe.y]
     // [closestPipe.x, pipe.y + pipe.height]
     const inputs = [
-      this.x / WIDTH,
-      this.y / HEIGHT,
+      Math.abs((this.x - pipeX) / WIDTH).toFixed(2),
+      (spaceStartY / HEIGHT).toFixed(2),
+      (spaceEndY / HEIGHT).toFixed(2),
+      (this.y / HEIGHT).toFixed(2),
+      (this.gravity / 3).toFixed(2),
     ];
+
 
     //range 0,1
     const output = this.brain.predict(inputs);
-    if (output[0] < 0.5) {
+    if (output[0] < output[1]) {
       this.jump();
     }
   }
 
+  mutate = () => {
+    this.brain.mutate((x) => {
+      if (Math.random() < 0.2) {
+        const offset = Math.random() * 0.5;
+        return x + offset;
+      }
+      return x;
+    });
+  }
 
 
   jump = () => {
-    this.gravity = -4;
+    this.gravity = -3;
   }
 }
 
+let counter = 0;
 class Pipe {
   constructor(ctx, height, space) {
     this.ctx = ctx;
@@ -97,12 +120,30 @@ class App extends Component {
     this.space = 120;
     this.birds = [];
     this.pipes = [];
+    this.deadBirds = [];
+    this.state = {
+      gameSpeed: FPS,
+      shouldDraw: false,
+    }
   }
   componentDidMount() {
     // document.addEventListener('keydown', this.onkeydown);
+    this.startGame();
+  }
+  startGame = () => {
+    counter++;
+    if (counter > 0) {
+      this.setState({ shouldDraw: true });
+    }
+    this.frameCount = 0;
+    clearInterval(this.loop);
+    const ctx = this.canvasRef.current.getContext('2d');
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+
     this.pipes = this.generatePipes();
     this.birds = this.generateBirds();
-    this.loop = setInterval(this.gameLoop, 1000 / FPS);
+    this.deadBirds = [];
+    this.loop = setInterval(this.gameLoop, 1000 / this.state.gameSpeed);
   }
 
   // USER ONLY MODE
@@ -125,18 +166,27 @@ class App extends Component {
     const birds = [];
     const ctx = this.getCtx();
     for (let i = 0; i < TOTAL_BIRDS; i += 1) {
-      birds.push(new Bird(ctx));
+      const brain = this.deadBirds.length && this.pickOne().brain;
+      const newBird = new Bird(ctx, brain);
+      birds.push(newBird);
     }
     return birds;
   };
 
   gameLoop = () => {
-    this.update();
+    if (this.state.shouldDraw) {
+      this.update();
+    }
+    else {
+      for (let i = 0; i < 10000; i++) {
+        this.update();
+      }
+    }
     this.draw();
   }
   update = () => {
     this.frameCount = this.frameCount + 1;
-    if (this.frameCount % 320 === 0) {
+    if ((this.frameCount % 240) === 0) {
       const pipes = this.generatePipes();
       this.pipes.push(...pipes);
     }
@@ -144,22 +194,56 @@ class App extends Component {
     this.pipes.forEach(pipe => pipe.update());
 
     //Update Birds Position
-    this.birds.forEach(bird => bird.update());
+    this.birds.forEach(bird => {
+      const nextPipe = this.getNextPipe(bird);
+      const spaceStartY = nextPipe.y + nextPipe.height;
+      bird.update(nextPipe.x, spaceStartY, spaceStartY + this.space)
+    });
 
     //delete off-screen pipes
     this.pipes = this.pipes.filter(pipe => !pipe.isDead);
 
     //delete dead birds
     this.updateBirdDeadState();
+    this.deadBirds.push(...this.birds.filter(bird => bird.isDead));
     this.birds = this.birds.filter(bird => !bird.isDead);
 
+    if (this.birds.length === 0) {
+      let totalAge = 0;
+
+      //Calculate Cumulative Age
+      this.deadBirds.forEach(deadBird => { totalAge += deadBird.age });
+
+      //Calculate Fitness Ratio
+      this.deadBirds.forEach(deadBird => { deadBird.fitness = deadBird.age / totalAge });
+      this.startGame();
+    }
+  }
+
+  pickOne = () => {
+    let index = 0;
+    let r = Math.random();
+    while (r > 0) {
+      r -= this.deadBirds[index] ? this.deadBirds[index].fitness : 0.1;
+      index += 1;
+    }
+    index -= 1;
+    return this.deadBirds[index];
+  }
+
+  getNextPipe = (bird) => {
+    for (let i = 0; i < this.pipes.length; i++) {
+      if (this.pipes[i].x > bird.x) {
+        return this.pipes[i];
+      }
+    }
   }
   updateBirdDeadState = () => {
     //Detect Collisions
     this.birds.forEach(bird => {
       this.pipes.forEach(pipe => {
         if (
-          bird.y < 0 || bird.y > HEIGHT ||
+          bird.y <= 0 || bird.y >= HEIGHT ||
           (bird.x >= pipe.x && bird.x <= pipe.x + pipe.width
             && bird.y >= pipe.y && bird.y <= pipe.y + pipe.height)) {
           bird.isDead = true;
@@ -168,6 +252,9 @@ class App extends Component {
     });
   }
   draw() {
+    if (!this.state.shouldDraw) {
+      return;
+    }
     const ctx = this.canvasRef.current.getContext('2d');
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
     this.pipes.forEach(pipe => pipe.draw());
@@ -183,7 +270,10 @@ class App extends Component {
           style={{ marginTop: '24px', border: '1px solid #c3c3c3' }}>
           Your Browser does not support the canvas element!
       </canvas>
-        <div onClick={() => this.setState({})} > {this.frameCount} </div>
+        <div>
+          <input type="range" min="1" max="1000" value={this.state.gameSpeed}
+            onChange={e => this.setState({ gameSpeed: e.target.value }, this.startGame)} />
+        </div>
       </div>
     );
   }
